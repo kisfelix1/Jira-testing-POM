41315df92294fe711bdab68f1d0537f426051949
kisfelix <kisfelix1@gmail.com>
2022-03-28 11:32:47 +0200

    Other - chromedriver.exe added

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -3,6 +3,7 @@ package base;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
