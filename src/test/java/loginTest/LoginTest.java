162e1b5bfbb548a28587758d9061cd36010f6eb2
kisfelix <kisfelix1@gmail.com>
2022-03-29 14:23:27 +0200

    Feature - Login tests

--- src/test/java/loginTest/LoginTest.java
+++ src/test/java/loginTest/LoginTest.java
@@ -1,16 +1,37 @@
 package loginTest;
 
 import base.BaseTests;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import pages.IndexPage;
+import util.WebDriverManager;
+
+import java.util.List;
 
 public class LoginTest extends BaseTests {
-    IndexPage loginPage = new IndexPage(driver);
+    IndexPage indexPage = new IndexPage(driver);
 
     @Test
-    public void successfulLoginTest() {
-        loginPage.setUsernameInputField(System.getenv("jira_username"));
-        loginPage.setPasswordInputField(System.getenv("jira_password"));
-        loginPage.clickLoginButton();
+    public void successfulLoginTestOnIndexPage() {
+        List<String> loginCredentials = indexPage.getLoginCredentials("valid");
+        indexPage.login(loginCredentials);
     }
+
+    @Test
+    public void emptyCredentialsTest() {
+        List<String> loginCredentials = indexPage.getLoginCredentials("empty");
+        indexPage.login(loginCredentials);
+        Assertions.assertEquals(0, 0);
+    }
+
+    @Test
+    public void wrongPasswordLoginTest(){
+        List<String> loginCredentials = indexPage.getLoginCredentials("invalid_password");
+        indexPage.login(loginCredentials);
+        WebDriverManager.waitUntilVisible(driver, indexPage.getWrongCredentials());
+        Assertions.assertEquals("Sorry, your username and password are incorrect - please try again.",
+                indexPage.getWrongCredentialsText(), "Messages do not match!");
+    }
+
+
 }
