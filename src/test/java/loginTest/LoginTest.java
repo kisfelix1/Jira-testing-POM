aaeb23a7d01a92a57918d9ea1a8dc5710964ca75
emis <emisnps@gmail.com>
2022-03-29 15:23:20 +0200

    Feature - successfullLoginTestOnIndexPage is implemented

--- src/test/java/loginTest/LoginTest.java
+++ src/test/java/loginTest/LoginTest.java
@@ -4,17 +4,23 @@ import base.BaseTests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import pages.IndexPage;
+import pages.ProfilePage;
 import util.WebDriverManager;
 
 import java.util.List;
 
 public class LoginTest extends BaseTests {
     IndexPage indexPage = new IndexPage(driver);
+    ProfilePage profilePage = new ProfilePage(driver);
 
     @Test
     public void successfulLoginTestOnIndexPage() {
         List<String> loginCredentials = indexPage.getLoginCredentials("valid");
         indexPage.login(loginCredentials);
+        WebDriverManager.waitUntilVisible(driver, indexPage.getUserIcon());
+        driver.get(indexPage.getUserProfilePageUrl());
+        WebDriverManager.waitUntilVisible(driver, profilePage.getUsername());
+        Assertions.assertEquals("automation16", profilePage.getUsernameText(), "Username do not match!");
     }
 
     @Test
