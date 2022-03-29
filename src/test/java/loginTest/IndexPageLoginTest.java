dce83b8efad01e7bfa6143c0a63f3fca5ea55cf7
BlaskoMate <blaskomate3@gmail.com>
2022-03-29 17:05:54 +0200

    Refactor - IndexLoginPage.java Test cases

--- src/test/java/loginTest/IndexPageLoginTest.java
+++ src/test/java/loginTest/IndexPageLoginTest.java
@@ -11,28 +11,16 @@ public class IndexPageLoginTest extends BaseLoginTest {
 
     @Test
     public void successfulLoginTestOnIndexPage() {
-        List<String> loginCredentials = indexPage.getLoginCredentials("valid");
-        indexPage.login(loginCredentials);
-        WebDriverManager.waitUntilVisible(driver, indexPage.getUserIcon());
-        driver.get(indexPage.getUserProfilePageUrl());
-        WebDriverManager.waitUntilVisible(driver, profilePage.getUsername());
-        Assertions.assertEquals("automation16", profilePage.getUsernameText(), "Username do not match!");
+        successfulLoginTest();
     }
 
     @Test
     public void emptyCredentialsTestOnIndexPage(){
-        indexPage.clickLoginButton();
-        WebDriverManager.waitUntilVisible(driver, indexPage.getWrongCredentials());
-        Assertions.assertEquals("Sorry, your username and password are incorrect - please try again.",
-                indexPage.getWrongCredentialsText(), "Messages do not match!");
+        emptyCredentialsTest();
     }
 
     @Test
     public void wrongPasswordLoginTestOnIndexPage(){
-        List<String> loginCredentials = indexPage.getLoginCredentials("invalid_password");
-        indexPage.login(loginCredentials);
-        WebDriverManager.waitUntilVisible(driver, indexPage.getWrongCredentials());
-        Assertions.assertEquals("Sorry, your username and password are incorrect - please try again.",
-                indexPage.getWrongCredentialsText(), "Messages do not match!");
+        wrongPasswordLoginTest();
     }
 }
