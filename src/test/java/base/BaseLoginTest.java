104b0f454b1331109058b97ad64e5d3a49b477d9
BlaskoMate <blaskomate3@gmail.com>
2022-03-30 10:32:30 +0200

    Refactor - rewrite test cases

--- src/test/java/base/BaseLoginTest.java
+++ src/test/java/base/BaseLoginTest.java
@@ -1,9 +1,6 @@
 package base;
 
-import org.junit.jupiter.api.Assertions;
 import util.WebDriverManager;
-
-import java.util.List;
 
 public class BaseLoginTest extends BaseLogin{
 
@@ -12,18 +9,27 @@ public class BaseLoginTest extends BaseLogin{
     }
 
 
-    public void emptyCredentialsTest(){
+    public void wrongPasswordLogin(){
+        indexPage.login("invalid_password");
+        waitForWrongLoginCredentialPopup();
+    }
+
+    public void emptyCredentialLogin() {
         indexPage.clickLoginButton();
+        waitForWrongLoginCredentialPopup();
+    }
+
+    private void waitForWrongLoginCredentialPopup(){
         WebDriverManager.waitUntilVisible(driver, indexPage.getWrongCredentials());
-        Assertions.assertEquals("Sorry, your username and password are incorrect - please try again.",
-                indexPage.getWrongCredentialsText(), "Messages do not match!");
     }
 
     public void wrongPasswordLoginTest() {
-        List<String> loginCredentials = indexPage.getLoginCredentials("invalid_password");
-        indexPage.login(loginCredentials);
-        WebDriverManager.waitUntilVisible(driver, indexPage.getWrongCredentials());
-        Assertions.assertEquals("Sorry, your username and password are incorrect - please try again.",
-                indexPage.getWrongCredentialsText(), "Messages do not match!");
+
+    }
+
+
+    public void openProfilePage(){
+        driver.get(indexPage.getUserProfilePageUrl());
+        WebDriverManager.waitUntilVisible(driver, profilePage.getUsername());
     }
 }
--- src/test/java/loginTest/IndexPageLoginTest.java
+++ src/test/java/loginTest/IndexPageLoginTest.java
@@ -1,6 +1,7 @@
 package loginTest;
 
 import base.BaseLoginTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import util.PageUrlCollection;
 
@@ -12,16 +13,25 @@ public class IndexPageLoginTest extends BaseLoginTest {
 
     @Test
     public void successfulLoginTestOnIndexPage() {
-        successfulLoginTest();
+        indexPage.successfulLogin();
+        openProfilePage();
+        Assertions.assertEquals("automation16", profilePage.getUsernameText(), "Username do not match!");
+
     }
 
     @Test
     public void emptyCredentialsTestOnIndexPage(){
-        emptyCredentialsTest();
+        emptyCredentialLogin();
+        Assertions.assertEquals("Sorry, your username and password are incorrect - please try again.",
+                indexPage.getWrongCredentialsText(), "Messages do not match!");
+        indexPage.successfulLogin();
     }
 
     @Test
     public void wrongPasswordLoginTestOnIndexPage(){
-        wrongPasswordLoginTest();
+        wrongPasswordLogin();
+        Assertions.assertEquals("Sorry, your username and password are incorrect - please try again.",
+                indexPage.getWrongCredentialsText(), "Messages do not match!");
+        indexPage.successfulLogin();
     }
 }
