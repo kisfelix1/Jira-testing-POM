4376eff0a38503c22cbc0898f8ce39e725732c3b
Krisztian Toth <tothkrisztian1569@gmail.com>
2022-03-30 17:17:19 +0200

    Feature - add new Tests to EditIssueTest.java

--- src/test/java/editIssue/EditIssueTest.java
+++ src/test/java/editIssue/EditIssueTest.java
@@ -3,7 +3,6 @@ package editIssue;
 import base.BaseEdit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import util.PageUrlCollection;
 
 public class EditIssueTest extends BaseEdit {
 
@@ -33,23 +32,74 @@ public class EditIssueTest extends BaseEdit {
     }
 
     @Test
-    public void editToucanIssueTest() {
+    public void editToucanIssue1Test() {
         login();
-
+        openIssue("TOUCAN", 1);
+        Assertions.assertTrue(editButtonIsVisible());
         logout();
     }
 
     @Test
-    public void editCoalaIssueTest() {
+    public void editToucanIssue2Test() {
         login();
-
+        openIssue("TOUCAN", 2);
+        Assertions.assertTrue(editButtonIsVisible());
         logout();
     }
 
     @Test
-    public void editJetiIssueTest() {
+    public void editToucanIssue3Test() {
         login();
+        openIssue("TOUCAN", 3);
+        Assertions.assertTrue(editButtonIsVisible());
+        logout();
+    }
 
+    @Test
+    public void editCoalaIssue1Test() {
+        login();
+        openIssue("COALA", 1);
+        Assertions.assertTrue(editButtonIsVisible());
+        logout();
+    }
+
+    @Test
+    public void editCoalaIssue2Test() {
+        login();
+        openIssue("COALA", 2);
+        Assertions.assertTrue(editButtonIsVisible());
+        logout();
+    }
+
+    @Test
+    public void editCoalaIssue3Test() {
+        login();
+        openIssue("COALA", 3);
+        Assertions.assertTrue(editButtonIsVisible());
+        logout();
+    }
+
+    @Test
+    public void editJetiIssue1Test() {
+        login();
+        openIssue("JETI", 1);
+        Assertions.assertTrue(editButtonIsVisible());
+        logout();
+    }
+
+    @Test
+    public void editJetiIssue2Test() {
+        login();
+        openIssue("JETI", 2);
+        Assertions.assertTrue(editButtonIsVisible());
+        logout();
+    }
+
+    @Test
+    public void editJetiIssue3Test() {
+        login();
+        openIssue("JETI", 3);
+        Assertions.assertTrue(editButtonIsVisible());
         logout();
     }
 }
