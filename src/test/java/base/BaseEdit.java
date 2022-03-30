c17d86534744422807eed4d560b8e2c4b93cfd29
Krisztian Toth <tothkrisztian1569@gmail.com>
2022-03-30 16:37:11 +0200

    Feature - add new methodes for edit cancel test in BaseEdit.java

--- src/test/java/base/BaseEdit.java
+++ src/test/java/base/BaseEdit.java
@@ -11,6 +11,12 @@ public class BaseEdit extends BaseTests{
     private final String OLD_TYPE = "Story";
     private final String NEW_TYPE = "Task";
 
+    private final String OLD_SUMMARY_FOR_CANCEL = "Issue not to edit";
+    private final String NEW_SUMMARY_FOR_CANCEL = "Edited";
+    private final String OLD_TYPE_FOR_CANCEL = "Story";
+    private final String NEW_TYPE_FOR_CANCEL = "Bug";
+
+
     public void clickEditButton(){
         issuePage.clickEditButton();
     }
@@ -19,6 +25,16 @@ public class BaseEdit extends BaseTests{
         issuePage.editIssueSummary(NEW_SUMMARY);
         issuePage.editIssueType(NEW_TYPE);
         issuePage.clickUpdateButton();
+    }
+
+    public void editIssueWithNewDataThenCancel(){
+        issuePage.editIssueSummary(NEW_SUMMARY_FOR_CANCEL);
+        issuePage.editIssueType(NEW_TYPE_FOR_CANCEL);
+        issuePage.clickCancelOnScreen();
+    }
+
+    public void clickOnAlert(){
+        issuePage.clickOnAlert();
     }
 
     public void revertIssueToOldData(){
@@ -37,7 +53,16 @@ public class BaseEdit extends BaseTests{
                 && issuePage.isCorrectType(NEW_TYPE);
     }
 
+    public boolean issueNotContainsNewDataAfterCancel(){
+        return issuePage.isCorrectSummary(OLD_SUMMARY_FOR_CANCEL)
+                && issuePage.isCorrectType(OLD_TYPE_FOR_CANCEL);
+    }
+
     public void openDummyIssuePage(){
         driver.get(PageUrlCollection.EDIT_DUMMY_ISSUE.getUrl());
+    }
+
+    public void openDummyIssuePageForCancel() {
+        driver.get(PageUrlCollection.CANCEL_EDIT_DUMMY_ISSUE.getUrl());
     }
 }
