fd62298704a9cc652a455858f8116227165fcf60
emis <emisnps@gmail.com>
2022-03-31 17:08:54 +0200

    Feature - browseJETI projects are implemented

--- src/test/java/browseIssueTest/BrowseIssueTest.java
+++ src/test/java/browseIssueTest/BrowseIssueTest.java
@@ -43,4 +43,29 @@ public class BrowseIssueTest extends BaseTests {
                 issuePage.getIssueKey(),
                 "Issue key is not matching");
     }
+
+    @Test
+    public void browseIssueJETI_ID_1() {
+        indexPage.goToURL(PageUrlCollection.JETI_ISSUE_ID_1.getUrl());
+        Assertions.assertEquals("JETI-1",
+                issuePage.getIssueKey(),
+                "Issue key is not matching");
+    }
+
+    @Test
+    public void browseIssueJETI_ID_2() {
+        indexPage.goToURL(PageUrlCollection.JETI_ISSUE_ID_2.getUrl());
+        Assertions.assertEquals("JETI-2",
+                issuePage.getIssueKey(),
+                "Issue key is not matching");
+    }
+
+    @Test
+    public void browseIssueJETI_ID_3() {
+        indexPage.goToURL(PageUrlCollection.JETI_ISSUE_ID_3.getUrl());
+        Assertions.assertEquals("JETI-3",
+                issuePage.getIssueKey(),
+                "Issue key is not matching");
+    }
+
 }
