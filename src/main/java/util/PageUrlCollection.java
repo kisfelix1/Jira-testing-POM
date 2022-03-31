79de9f8ee758852d799acd7720708d450aadfb63
emis <emisnps@gmail.com>
2022-03-31 11:14:36 +0200

    Fix - add MTP_ISSUE url

--- src/main/java/util/PageUrlCollection.java
+++ src/main/java/util/PageUrlCollection.java
@@ -3,7 +3,8 @@ package util;
 public enum PageUrlCollection {
     INDEX("https://jira-auto.codecool.metastage.net/secure/Dashboard.jspa"),
     LOGIN("https://jira-auto.codecool.metastage.net/login.jsp"),
-    PROFIL("https://jira-auto.codecool.metastage.net/secure/ViewProfile.jspa");
+    PROFIL("https://jira-auto.codecool.metastage.net/secure/ViewProfile.jspa"),
+    MTP_ISSUE("https://jira-auto.codecool.metastage.net/issues/?jql=project%20%3D%20%22Main%20Testing%20Project%22%20AND%20summary%20~%20MTP-8982431");
 
     private final String url;
 
