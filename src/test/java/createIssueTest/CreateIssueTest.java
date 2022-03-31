5177f5d034d005d2d41497402232c9995a8bf96d
emis <emisnps@gmail.com>
2022-03-31 11:09:10 +0200

    Feature - cancelIssueTest is implemented

--- src/test/java/createIssueTest/CreateIssueTest.java
+++ src/test/java/createIssueTest/CreateIssueTest.java
@@ -5,9 +5,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pages.IndexPage;
-import pages.IssuePage;
+import util.PageUrlCollection;
 
-import java.io.IOException;
 
 public class CreateIssueTest extends BaseIssueTest {
     @BeforeEach
@@ -21,10 +20,25 @@ public class CreateIssueTest extends BaseIssueTest {
         indexPage.fillProjectInputField();
         indexPage.fillSummaryInputField();
         indexPage.clickCreateIssueButton();
-        indexPage.navigateToURL(IndexPage.issueURL);
+        indexPage.navigateToURL(PageUrlCollection.MTP_ISSUE.getUrl());
         Assertions.assertEquals("MTP-8982431",
                 indexPage.getSummaryIssueName(),
-                "Summaries do not match");
+                "Summaries do not match.");
         indexPage.deleteIssue();
+    }
+
+    @Test
+    public void cancelIssueTest(){
+        indexPage.fillProjectInputField();
+        indexPage.fillSummaryInputField();
+        indexPage.clickToCancelButton();
+        indexPage.clickToAlert();
+        indexPage.goToURL(PageUrlCollection.MTP_ISSUE.getUrl());
+        Assertions.assertEquals("No issues were found to match your search",
+                issuePage.getIssuesH2Text(),
+                "The text does not matching.");
+
+
+
     }
 }
