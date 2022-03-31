46aeff3e3a2160e72b60d3f0010eab553199f602
emis <emisnps@gmail.com>
2022-03-31 09:21:18 +0200

    Feature - add project urls

--- src/main/java/util/PageUrlCollection.java
+++ src/main/java/util/PageUrlCollection.java
@@ -3,7 +3,12 @@ package util;
 public enum PageUrlCollection {
     INDEX("https://jira-auto.codecool.metastage.net/secure/Dashboard.jspa"),
     LOGIN("https://jira-auto.codecool.metastage.net/login.jsp"),
-    PROFIL("https://jira-auto.codecool.metastage.net/secure/ViewProfile.jspa");
+    PROFIL("https://jira-auto.codecool.metastage.net/secure/ViewProfile.jspa"),
+    MTP_PROJECT("https://jira-auto.codecool.metastage.net/projects/MTP/summary"),
+    COALA_PROJECT("https://jira-auto.codecool.metastage.net/projects/COALA/summary"),
+    TOUCAN_PROJECT("https://jira-auto.codecool.metastage.net/projects/TOUCAN/summary"),
+    JETI_PROJECT("https://jira-auto.codecool.metastage.net/projects/JETI/summary");
+
 
     private final String url;
 
