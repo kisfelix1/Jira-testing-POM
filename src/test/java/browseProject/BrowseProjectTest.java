d5960825543cbfae152045424f686c8af332649a
emis <emisnps@gmail.com>
2022-03-31 09:20:41 +0200

    Refactor - remake browse test cases

--- src/test/java/browseProject/BrowseProjectTest.java
+++ src/test/java/browseProject/BrowseProjectTest.java
@@ -4,6 +4,7 @@ import base.BaseTests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import pages.ProjectPage;
+import util.PageUrlCollection;
 
 
 public class BrowseProjectTest extends BaseTests {
@@ -14,32 +15,40 @@ public class BrowseProjectTest extends BaseTests {
     @Test
     public void browseMTPProject(){
         login();
-        driver.get(ProjectPage.MTP_PATH);
-        Assertions.assertEquals("MTP", projectPage.getProjectKeyText(), "Key is not matching");
+        projectPage.navigateToNextUrl(PageUrlCollection.MTP_PROJECT.getUrl());
+        Assertions.assertEquals("MTP",
+                projectPage.getProjectKeyText(),
+                "Key is not matching");
         logout();
     }
 
     @Test
     public void browseTOUCANProject(){
         login();
-        driver.get(ProjectPage.TOUCAN_PATH);
-        Assertions.assertEquals("TOUCAN", projectPage.getProjectKeyText(), "Key is not matching");
+        projectPage.navigateToNextUrl(PageUrlCollection.TOUCAN_PROJECT.getUrl());
+        Assertions.assertEquals("TOUCAN",
+                projectPage.getProjectKeyText(),
+                "Key is not matching");
         logout();
     }
 
     @Test
     public void browseJETIProject(){
         login();
-        driver.get(ProjectPage.JETI_PATH);
-        Assertions.assertEquals("JETI", projectPage.getProjectKeyText(), "Key is not matching");
+        projectPage.navigateToNextUrl(PageUrlCollection.JETI_PROJECT.getUrl());
+        Assertions.assertEquals("JETI",
+                projectPage.getProjectKeyText(),
+                "Key is not matching");
         logout();
     }
 
     @Test
     public void browseCOALAProject(){
         login();
-        driver.get(ProjectPage.COALA_PATH);
-        Assertions.assertEquals("COALA", projectPage.getProjectKeyText(), "Key is not matching");
+        projectPage.navigateToNextUrl(PageUrlCollection.COALA_PROJECT.getUrl());
+        Assertions.assertEquals("COALA",
+                projectPage.getProjectKeyText(),
+                "Key is not matching");
         logout();
     }
 
