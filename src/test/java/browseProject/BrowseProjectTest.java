32c55fb643e0cd2bc13964e48207163da1d35e62
emis <emisnps@gmail.com>
2022-04-10 17:11:13 +0200

    Refactor - use ParameterizedTest for browsing projects, deleted four method

--- src/test/java/browseProject/BrowseProjectTest.java
+++ src/test/java/browseProject/BrowseProjectTest.java
@@ -2,46 +2,21 @@ package browseProject;
 
 import base.BaseTests;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import pages.ProjectPage;
-import util.PageUrlCollection;
 
 
 public class BrowseProjectTest extends BaseTests {
 
     private final ProjectPage projectPage = new ProjectPage(driver);
 
-
-    @Test
-    public void browseMTPProject(){
-        projectPage.navigateToNextUrl(PageUrlCollection.MTP_PROJECT.getUrl());
-        Assertions.assertEquals("MTP",
+    @ParameterizedTest
+    @ValueSource(strings = {"MTP","TOUCAN","JETI","COALA"})
+    public void browseAllProjectTest(String input){
+        projectPage.navigateToNextUrl(String.format("https://jira-auto.codecool.metastage.net/projects/%s/summary", input));
+        Assertions.assertEquals(input,
                 projectPage.getProjectKeyText(),
                 "Key is not matching");
     }
-
-    @Test
-    public void browseTOUCANProject(){
-        projectPage.navigateToNextUrl(PageUrlCollection.TOUCAN_PROJECT.getUrl());
-        Assertions.assertEquals("TOUCAN",
-                projectPage.getProjectKeyText(),
-                "Key is not matching");
-    }
-
-    @Test
-    public void browseJETIProject(){
-        projectPage.navigateToNextUrl(PageUrlCollection.JETI_PROJECT.getUrl());
-        Assertions.assertEquals("JETI",
-                projectPage.getProjectKeyText(),
-                "Key is not matching");
-    }
-
-    @Test
-    public void browseCOALAProject(){
-        projectPage.navigateToNextUrl(PageUrlCollection.COALA_PROJECT.getUrl());
-        Assertions.assertEquals("COALA",
-                projectPage.getProjectKeyText(),
-                "Key is not matching");
-    }
-
 }
