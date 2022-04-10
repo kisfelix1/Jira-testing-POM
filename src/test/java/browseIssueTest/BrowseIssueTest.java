b38ff8be5b52670f58bfb456208c22ec51b13490
emis <emisnps@gmail.com>
2022-04-10 18:37:38 +0200

    Refactor - use ParameterizedTest for browse issues, deleting unnecessary methods

--- src/test/java/browseIssueTest/BrowseIssueTest.java
+++ src/test/java/browseIssueTest/BrowseIssueTest.java
@@ -1,95 +1,23 @@
 package browseIssueTest;
 
-import base.BaseBrowseIssueTest;
 import base.BaseTests;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import pages.IndexPage;
 import pages.IssuePage;
-import util.PageUrlCollection;
 
-public class BrowseIssueTest extends BaseBrowseIssueTest {
+public class BrowseIssueTest extends BaseTests {
 
+    IndexPage indexPage = new IndexPage(driver);
+    IssuePage issuePage = new IssuePage(driver);
 
-    @Test
-    public void browseIssue(){
-        indexPage.goToURL(PageUrlCollection.MTP_ISSUE_FOR_BROWSE.getUrl());
-        Assertions.assertEquals(MTP_ISSUE_KEY,
+    @ParameterizedTest
+    @CsvFileSource(resources = "/issueName.csv", numLinesToSkip = 1)
+    public void browseAllIssue(String input, String projectName, String expected){
+        indexPage.goToURL(String.format("https://jira-auto.codecool.metastage.net/projects/%s/issues/%s?filter=allopenissues",projectName, input));
+        Assertions.assertEquals(expected,
                 issuePage.getIssueKey(),
                 "Issue key is not matching");
     }
-
-    @Test
-    public void browseIssueCOALA_ID_1(){
-        indexPage.goToURL(PageUrlCollection.COALA_ISSUE_ID_1.getUrl());
-        Assertions.assertEquals(COALA_ISSUE_KEY_1,
-                issuePage.getIssueKey(),
-                "Issue key is not matching");
-    }
-
-    @Test
-    public void browseIssueCOALA_ID_2(){
-        indexPage.goToURL(PageUrlCollection.COALA_ISSUE_ID_2.getUrl());
-        Assertions.assertEquals(COALA_ISSUE_KEY_2,
-                issuePage.getIssueKey(),
-                "Issue key is not matching");
-    }
-
-    @Test
-    public void browseIssueCOALA_ID_3(){
-        indexPage.goToURL(PageUrlCollection.COALA_ISSUE_ID_3.getUrl());
-        Assertions.assertEquals(COALA_ISSUE_KEY_3,
-                issuePage.getIssueKey(),
-                "Issue key is not matching");
-    }
-
-    @Test
-    public void browseIssueJETI_ID_1() {
-        indexPage.goToURL(PageUrlCollection.JETI_ISSUE_ID_1.getUrl());
-        Assertions.assertEquals(JETI_ISSUE_KEY_1,
-                issuePage.getIssueKey(),
-                "Issue key is not matching");
-    }
-
-    @Test
-    public void browseIssueJETI_ID_2() {
-        indexPage.goToURL(PageUrlCollection.JETI_ISSUE_ID_2.getUrl());
-        Assertions.assertEquals(JETI_ISSUE_KEY_2,
-                issuePage.getIssueKey(),
-                "Issue key is not matching");
-    }
-
-    @Test
-    public void browseIssueJETI_ID_3() {
-        indexPage.goToURL(PageUrlCollection.JETI_ISSUE_ID_3.getUrl());
-        Assertions.assertEquals(JETI_ISSUE_KEY_3,
-                issuePage.getIssueKey(),
-                "Issue key is not matching");
-    }
-
-    @Test
-    public void browseIssueTOUCAN_ID_1() {
-        indexPage.goToURL(PageUrlCollection.TOUCAN_ISSUE_ID_1.getUrl());
-        Assertions.assertEquals(TOUCAN_ISSUE_KEY_1,
-                issuePage.getIssueKey(),
-                "Issue key is not matching");
-    }
-
-    @Test
-    public void browseIssueTOUCAN_ID_2() {
-        indexPage.goToURL(PageUrlCollection.TOUCAN_ISSUE_ID_2.getUrl());
-        Assertions.assertEquals(TOUCAN_ISSUE_KEY_2,
-                issuePage.getIssueKey(),
-                "Issue key is not matching");
-    }
-
-    @Test
-    public void browseIssueTOUCAN_ID_3() {
-        indexPage.goToURL(PageUrlCollection.TOUCAN_ISSUE_ID_3.getUrl());
-        Assertions.assertEquals(TOUCAN_ISSUE_KEY_3,
-                issuePage.getIssueKey(),
-                "Issue key is not matching");
-    }
-
-
 }
